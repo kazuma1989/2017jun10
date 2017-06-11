f9de8f2532ab6d018df6fa0c17c54fec6bc8e1ea
kazuma1989 <funifuni.1204@gmail.com>
2017-06-11 05:00:32 +0000

    Router 学習のため page1 コンポーネントを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
@@ -7,13 +8,22 @@ import { MaterialModule } from '@angular/material';
 import { HotTableModule } from 'ng2-handsontable';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        Page1Component
     ],
     imports: [
         BrowserModule,
+        RouterModule.forRoot([
+          {
+            path: 'page1',
+            component: Page1Component
+          }
+        ]),
         HttpModule,
         FormsModule,
         MaterialModule,

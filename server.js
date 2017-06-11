615eb505e30e8a3583cbef8a1fabfe376a3e4263
kazuma1989 <funifuni.1204@gmail.com>
2017-06-11 12:25:23 +0000

    POST するとファイルを生成する仕組みを追加した

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 app.use(express.static('dist'));
@@ -7,6 +11,20 @@ app.get('/api/login', (req, resp) => {
     resp.json({
         status: 'ok'
     });
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/api/edit/:mdFile', (req, resp) => {
+    console.log(req.params.mdFile);
+    console.log(req.body);
+
+    fs.writeFile(path.join(__dirname, 'dist', req.params.mdFile), req.body.content);
+
+    resp.send('POST!!');
 });
 
 const port = 8081;

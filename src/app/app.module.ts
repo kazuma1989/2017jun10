274c8a42a3f1a4e202fe62a5f697b56061b6b34d
kazuma1989 <funifuni.1204@gmail.com>
2017-06-11 05:14:59 +0000

    page2 も追加した

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HotTableModule } from 'ng2-handsontable';
 
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        Page1Component
+        Page1Component,
+        Page2Component
     ],
     imports: [
         BrowserModule,
@@ -22,7 +24,11 @@ import { Page1Component } from './page1/page1.component';
           {
             path: 'page1',
             component: Page1Component
-          }
+          },
+          {
+            path: 'page2',
+            component: Page2Component
+          },
         ]),
         HttpModule,
         FormsModule,

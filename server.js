25337df71d9647562d98a2c32dbc183f0477e118
kazuma1989 <funifuni.1204@gmail.com>
2017-06-13 20:25:51 +0900

    Express サーバーを使うと直接 Angular のルーティングを呼び出したときに 404 になってしまうのを解消した

--- server.js
+++ server.js
@@ -5,20 +5,24 @@ const path = require('path');
 
 const app = express();
 
-app.use('/data', express.static(__dirname + '/data'));
+// ビルドした資材を配布する設定
 app.use(express.static('dist'));
 
-app.get('/api/login', (req, resp) => {
-    resp.json({
-        status: 'ok'
-    });
+// Static ファイルが見つからないときは Angular のルーティングを呼び出す
+app.get('*', (req, resp) => {
+    resp.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+// Markdown ファイルを呼び出せるようにする
+app.use('/data', express.static(__dirname + '/data'));
+
+// JSON による POST リクエストに対応する
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+// Markdown ファイルの投稿 API
 app.post('/api/edit/:mdFile', (req, resp) => {
     console.log(req.params.mdFile);
     console.log(req.body);
@@ -32,6 +36,7 @@ app.post('/api/edit/:mdFile', (req, resp) => {
     resp.send('POST!!');
 });
 
+// 固定ポートでサーバーを起動
 const port = 8081;
 app.listen(port, () => {
     console.log('app running on port', port);
